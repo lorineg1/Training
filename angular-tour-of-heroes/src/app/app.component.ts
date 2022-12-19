3f4776c5b12f4c8fe7f182bb522417f32c54d0cd
omega6team <omega.six.team@gmail.com>
2022-12-19 10:34:33 +0200

    Check webhook

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -9,4 +9,5 @@ export class AppComponent {
   title = 'Mazal Tov';
   bracha = "כל הכבוד על עליית האתר. אתם בחצי הדרך בלסיים את הפרק ועוד מעט תיכנסו לצוות אומגה";
   end = "וכמו שחוכמי אומגה אמרו: 'בחורה לחתונה מוצאים רק בספריה' ע.ר"
+  checkwebhook = "Hello im checking this webhook"
 }
