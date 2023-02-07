5503276e9d6ca552bfe7e1c6f0c75b057e021055
lorineg1 <lorinegrinberg@yahoo.com>
2023-02-07 10:56:51 +0200

    check

--- Lorine Python/Shopping!/backend/mysite/shop/models.py
+++ Lorine Python/Shopping!/backend/mysite/shop/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from django.utils import timezone
+
 class Category(models.Model):
     name = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
     name = models.CharField(max_length = 50)
@@ -11,3 +15,8 @@ class Product(models.Model):
     image = models.ImageField(upload_to="shop/")
     category = models.ForeignKey("Category",  on_delete=models.CASCADE) 
 
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reversed('product -detail', args=[str(self.id)])
--- Lorine Python/Shopping!/backend/mysite/shop/views.py
+++ Lorine Python/Shopping!/backend/mysite/shop/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product, Category
 
 def index(request):
-    return HttpResponse("Hi, Welcome to My Store")
+    num_products = Product.objects.all().count()
+    return HttpResponse("Hi, Welcome to My Store, I smell like shit")
 
 def viewProduct(request, id):
-    return HttpResponse(id)
+    response = "You are looking at Category."
+    Product_Category = Product.objects.filter(category = 'shirts')
+    return HttpResponse(response % id)
+
+def home(request):
+    category = Category.objects.all().filter(parent=None)
+    post_by_category = Product.objects.filter(category__category_name = "aaa", published = True)
+    slider = Product.objects.filter(slider = True).order_by('-created_on')
+    context = {
+        'category' : category,
+        'post_by_category' : post_by_category,
+        'slider' : slider,
+    } 
+    return render(request, 'home.html', context)
